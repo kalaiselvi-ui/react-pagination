a64cf2d6de4232ba39b4bd1e43a9c6c8d38072e1
Kalaiselvi Kalai <kalaichandran369@gmail.com>
2022-07-08 23:02:58 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import PaginationTwo from './api/PaginationTwo';
+import './App.css';
+import PaginationOne from './PaginationOne';
+import AxiosApi from './AxiosApi'
+
+function App() {
+  return (
+    <div className="App">
+     {/* <PaginationOne /> */}
+     <PaginationTwo />
+     {/* <AxiosApi /> */}
+    </div>
+  );
+}
+
+export default App;
